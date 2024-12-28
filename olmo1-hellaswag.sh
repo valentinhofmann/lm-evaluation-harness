3e07665c1c683700864ece13bf6c70f77e04e5bc
Valentin Hofmann <valentinhofmann@gmx.de>
2024-12-28 15:11:57 +0100

    Added HellaSwag script

---
+++ olmo1-hellaswag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gpu_count=$(nvidia-smi -L | wc -l)
+
+while read r
+do
+    lm_eval --model vllm \
+    --model_args pretrained=allenai/OLMo-7B-0724-hf,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.8 \
+    --tasks hellaswag \
+    --num_fewshot 10 \
+    --batch_size auto \
+    --log_samples \
+    --output_path results
+done <olmo1-revisions.txt
