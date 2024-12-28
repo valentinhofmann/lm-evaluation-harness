031e9e4a72adbf83c70de9ed50bb13c2b73be3b1
Valentin Hofmann <valentinhofmann@gmx.de>
2024-12-28 16:05:50 +0100

    Updated OLL scripts

--- olmo1-arc-challenge.sh
+++ olmo1-arc-challenge.sh
@@ -5,7 +5,7 @@ gpu_count=$(nvidia-smi -L | wc -l)
 while read r
 do
     lm_eval --model vllm \
-    --model_args pretrained=allenai/OLMo-7B-0724-hf,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.8 \
+    --model_args pretrained=allenai/OLMo-7B-0724-hf,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.7 \
     --tasks arc_challenge \
     --num_fewshot 25 \
     --batch_size auto \
