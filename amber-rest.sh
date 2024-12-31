46cbd6eb37928b0bb58e463df2d7a81275f9f78c
Valentin Hofmann <valentinhofmann@gmx.de>
2024-12-31 22:07:51 +0100

    Added Amber scripts

---
+++ amber-rest.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+gpu_count=$(nvidia-smi -L | wc -l)
+
+while read r
+do
+    lm_eval --model vllm \
+    --model_args pretrained=LLM360/Amber,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.7 \
+    --tasks arc_challenge \
+    --num_fewshot 25 \
+    --batch_size auto \
+    --log_samples \
+    --output_path results
+
+    lm_eval --model vllm \
+    --model_args pretrained=LLM360/Amber,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.7 \
+    --tasks gsm8k \
+    --num_fewshot 5 \
+    --batch_size auto \
+    --log_samples \
+    --output_path results
+
+    lm_eval --model vllm \
+    --model_args pretrained=LLM360/Amber,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.7 \
+    --tasks truthfulqa_mc2 \
+    --num_fewshot 0 \
+    --batch_size auto \
+    --log_samples \
+    --output_path results
+
+    lm_eval --model vllm \
+    --model_args pretrained=LLM360/Amber,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.7 \
+    --tasks winogrande \
+    --num_fewshot 5 \
+    --batch_size auto \
+    --log_samples \
+    --output_path results
+done <amber-revisions.txt
