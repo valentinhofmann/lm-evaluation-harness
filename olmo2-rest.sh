fbd95bfaf07a45c72a285d3e53df77d2afb28424
Valentin Hofmann <valentinhofmann@gmx.de>
2025-01-02 14:12:53 +0100

    Added OLMo2 scripts

--- olmo2-rest.sh
+++ olmo2-rest.sh
@@ -21,7 +21,7 @@ do
     --output_path results_olmo2
 
     lm_eval --model vllm \
-    --model_args pretrained=allenai/OLMo-2-1124-7B,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.6 \
+    --model_args pretrained=allenai/OLMo-2-1124-7B,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.5 \
     --tasks truthfulqa_mc2 \
     --num_fewshot 0 \
     --batch_size auto \
