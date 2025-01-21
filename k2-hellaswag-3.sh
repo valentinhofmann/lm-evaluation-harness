c8f64edcae341157007a2dd1928fab6599014b23
Valentin Hofmann <valentinhofmann@gmx.de>
2025-01-21 09:55:10 -0800

    Added scripts for third part of K2 eval (HellaSwag)

---
+++ k2-hellaswag-3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gpu_count=$(nvidia-smi -L | wc -l)
+
+while read r
+do
+    lm_eval --model vllm \
+    --model_args pretrained=LLM360/K2,revision=$r,tensor_parallel_size=$gpu_count,dtype=auto,gpu_memory_utilization=0.8 \
+    --tasks hellaswag \
+    --num_fewshot 10 \
+    --batch_size auto \
+    --log_samples \
+    --output_path results_k2
+done <k2-revisions-3-hellaswag.txt
